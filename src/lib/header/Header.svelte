4bafd05f4a2b1b4fdec221ff001ae615faaa5ceb
DUONG Phu-Hiep <duongphuhiep@gmail.com>
2022-06-20 08:05:06 +0200

    add i18n

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,21 +1,31 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import logo from './svelte-logo.svg';
+    import {_, locale, locales} from 'svelte-i18n'
 </script>
 
 <header>
+    
+    
 	<div class="corner">
 		<a href="https://kit.svelte.dev">
 			<img src={logo} alt="SvelteKit" />
 		</a>
 	</div>
 
+    <div>
+        <select bind:value={$locale}>
+            {#each $locales as locale}
+              <option value={locale}>{locale}</option>
+            {/each}
+          </select>
+    </div>
 	<nav>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
 		</svg>
 		<ul>
-			<li class:active={$page.url.pathname === '/'}><a sveltekit:prefetch href="/">Home</a></li>
+			<li class:active={$page.url.pathname === '/'}><a sveltekit:prefetch href="/">{$_("home")}</a></li>
 			<li class:active={$page.url.pathname === '/about'}>
 				<a sveltekit:prefetch href="/about">About</a>
 			</li>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,45 +1,77 @@
-<script lang="ts">
-	import Header from '$lib/header/Header.svelte';
-	import '../app.css';
+<script context="module" lang="ts">
+  import {
+    register,
+    init,
+    getLocaleFromNavigator,
+    isLoading,
+  } from "svelte-i18n";
+
+  register("en", () => import("../locales/en.json"));
+  register("fr", () => import("../locales/fr.json"));
+  register("vn", () => import("../locales/vn.json"));
+  init({
+    fallbackLocale: "en",
+    initialLocale: getLocaleFromNavigator(),
+  });
+
+  import { waitLocale } from "svelte-i18n";
+
+  export async function preload() {
+    // awaits for the loading of the 'en-US' and 'en' dictionaries
+    return waitLocale();
+  }
 </script>
 
-<Header />
+<script lang="ts">
+  import Header from "$lib/header/Header.svelte";
+  import "../app.css";
+  import { _ } from "svelte-i18n";
+</script>
 
-<main>
-	<slot />
-</main>
+{#if $isLoading}
+  Loading language, please wait..
+{:else}
+  <Header />
 
-<footer>
-	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-</footer>
+  <main>
+    <slot />
+  </main>
+
+  <footer>
+    <p>
+      {$_("home")} visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn
+      SvelteKit
+    </p>
+  </footer>
+{/if}
 
 <style>
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 1024px;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
+  main {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 1rem;
+    width: 100%;
+    max-width: 1024px;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
 
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 40px;
-	}
+  footer {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 40px;
+  }
 
-	footer a {
-		font-weight: bold;
-	}
+  footer a {
+    font-weight: bold;
+  }
 
-	@media (min-width: 480px) {
-		footer {
-			padding: 40px 0;
-		}
-	}
+  @media (min-width: 480px) {
+    footer {
+      padding: 40px 0;
+    }
+  }
 </style>
